614d76b3e3dcf4fff6235f0a92f4b13998f8a94d
Lucas Samuel <lukssds@gmail.com>
2022-05-08 22:11:07 -0300

    Finalizando algoritimo

--- src/perceptron_digitos/Aprendizagem.java
+++ src/perceptron_digitos/Aprendizagem.java
@@ -1,23 +1,50 @@
 package perceptron_digitos;
-
+/**
+ *
+ * @author Clarimundo
+ */
 public class Aprendizagem {
     
     private double x[][]={{1,1,1,1,1,0,1,1,0,1,1,0,1,1,1,1},
-                          {1,0,1,0,1,1,0,0,1,0,0,1,0,1,1,1}};
+                          {1,0,1,0,1,1,0,0,1,0,0,1,0,1,1,1},
+                          {1,1,1,1,0,0,1,1,1,1,1,0,0,1,1,1},
+                          {1,1,1,1,0,0,1,1,1,1,0,0,1,1,1,1},
+                          {1,1,0,1,1,0,1,1,1,1,0,0,1,0,0,1},
+                          {1,1,1,1,1,0,0,1,1,1,0,0,1,1,1,1},
+                          {1,1,1,1,1,0,0,1,1,1,1,0,1,1,1,1},
+                          {1,1,1,1,0,0,1,0,0,1,0,0,1,0,0,1},
+                          {1,1,1,1,1,0,1,1,1,1,1,0,1,1,1,1},
+                          {1,1,1,1,1,0,1,1,1,1,0,0,1,1,1,1}};
    
-    private double w[]= new double [16];
-    private double t[]={1,-1};       
+    private double w[][]= new double [16][4];
+    private double t[][]={{1,1,1,1},
+{1,1,1,-1},
+{1,1,-1,1},
+{1,1,-1,-1},
+{1,-1,1,1},
+{1,-1,1,-1},
+{1,-1,-1,1},
+{1,-1,-1,-1},
+{-1,1,1,1},
+{-1,1,1,-1},
+{-1,1,-1,1},
+{-1,1,-1,-1},
+{-1,-1,1,1},
+{-1,-1,1,1},
+{-1,-1,1,-1},
+{-1,-1,-1,1},
+{-1,-1,-1,-1}};      
     private int epocas;
         
     public Aprendizagem(){  
         epocas=0;
     }
     
-    public double [] getw(){
+    public double [][] getw(){
         return w;
     }
     
-    public double [] gett(){
+    public double [][] gett(){
         return t;
     }    
     
@@ -25,50 +52,57 @@ public class Aprendizagem {
         return epocas;
     }
     
-    public double somatorio(int i){
-        double yent=0;       
-        for(int j=0;j<16;j++)
-            yent = yent + x[i][j]*w[j];
-        return yent;
+    public double somatorio(int i,int k){
+        double yent[][]= new double[10][4];       
+                for(int j=0;j<16;j++){
+                    yent[i][k] = yent[i][k] + x[i][j]*w[j][k];
+                }
+            return yent[i][k];
     }
 
     public double saida(double yent, double limiar){
        double f;
        
-        if(yent > limiar)
+          if(yent > limiar)
             f = 1;
-        else 
-            if(yent < -limiar)
-                f = -1;          
-            else
+        else if(yent < -limiar)
+                f = -1;
+        else
                 f = 0;
        return f;
     }
     
-    public void atualiza(double alfa, double f[]){
-            for(int i=0;i<2;i++)
-                for(int j=0; j<16; j++)
-                     w[j] = w[j] + alfa * (t[i]-f[i]) * x[i][j];
+    public void atualiza(double alfa,double f[][],int k){
+                   for(int i=0;i<4;i++)
+                    for(int j=0; j<16; j++)
+                            w[j][i] = w[j][i] + alfa * (t[k][i]-f[k][i]) * x[k][j];
     }
     
     public void algoritmo(double alfa, double limiar){
-       double yent;
-       double f[] = {0,0};
+       double yent[][] = new double[10][4];
+       double f[][] = {{0,0,0,0},{0,0,0,0},{0,0,0,0},{0,0,0,0},{0,0,0,0},{0,0,0,0},
+       {0,0,0,0},{0,0,0,0},{0,0,0,0},{0,0,0,0}};
        boolean mudou;       
        
-       for(int j=0;j<16;j++)  // zerar os pesos
-               w[j]=0;       
+       for(int i=0;i<16;i++)
+            for(int j=0;j<4;j++)
+               w[i][j]=0;       
        do{
            mudou=false;
-           for(int i=0;i<2;i++){   // dois padrões de entrada (digito 0) e (digito 1)
-              yent = somatorio(i);
-              f[i] = saida(yent,limiar);
-              if(f[i] != t[i])                  
-                  mudou=true;
-           }
-           if(mudou==true)
-              atualiza(alfa,f); 
-           epocas++;
+          
+            for(int i=0;i<10;i++){
+              for(int j=0;j<4;j++){
+              yent[i][j] = somatorio(i,j);
+              f[i][j] = saida(yent[i][j],limiar);
+                  if(f[i][j] != t[i][j]){                  
+                     mudou=true;
+                  }
+                  if(mudou){
+                  atualiza(alfa,f,i); 
+                  epocas++;
+                 }
+              }
+            }
        }while(mudou==true);      
-    }      
+    }   
 }
--- src/perceptron_digitos/Validacao.java
+++ src/perceptron_digitos/Validacao.java
@@ -1,14 +1,17 @@
 package perceptron_digitos;
-
+/**
+ *
+ * @author Clarimundo
+ */
 public class Validacao {
     
     public Validacao(){
         
     }
     
-    public double somatorio(int mat[][], double w[]){
+    public double[] somatorio(int mat[][], double w[][]){
         
-        double yent=0;  
+        double [] yent= new double[4];
         double entrada[] = new double[16];
         int l=1;
         entrada[0]=1;
@@ -17,9 +20,11 @@ public class Validacao {
                 entrada[l] = mat[i][j];
                 l++;
             }        
-        for(int j=1;j<16;j++)
-            yent = yent + entrada[j]*w[j];
-        return yent;
+        for(int j=0;j<16;j++)
+            for(int i=0;i<4;i++)
+               yent[i] = yent[i] + entrada[j]*w[j][i];
+        
+         return yent;
     }
 
     public double saida(double yent, double limiar){
@@ -35,16 +40,44 @@ public class Validacao {
         return f;
     }
     
-    public String teste(int mat[][], double w[], double t[], double limiar){
-              
-       double yent = somatorio(mat,w);
-       double f = saida(yent,limiar);
-       if(f==t[0])
+    public String teste(int mat[][], double w[][], double t[][], double limiar){
+       double [] f = new double[4];
+       double [] yent = new double[4];
+               
+       yent = somatorio(mat,w);
+       for(int i=0;i<4;i++){
+        f[i] = saida(yent[i],limiar);
+       }
+       if(f[0]==t[0][0] && f[1] == t[0][1] && f[2] == t[0][2] && f[3] == t[0][3])
            return "0";
        else
-           if(f==t[1])
+          if(f[0]==t[1][0] && f[1] == t[1][1] && f[2] == t[1][2] && f[3] == t[1][3])
                return "1";
-           else
+       else
+          if(f[0]==t[2][0] && f[1] == t[2][1] && f[2] == t[2][2] && f[3] == t[2][3])
+               return "2";
+       else
+          if(f[0]==t[3][0] && f[1] == t[3][1] && f[2] == t[3][2] && f[3] == t[3][3])
+               return "3";
+       else
+          if(f[0]==t[4][0] && f[1] == t[4][1] && f[2] == t[4][2] && f[3] == t[4][3])
+               return "4";
+       else
+          if(f[0]==t[5][0] && f[1] == t[5][1] && f[2] == t[5][2] && f[3] == t[5][3])
+               return "5";
+       else
+          if(f[0]==t[6][0] && f[1] == t[6][1] && f[2] == t[6][2] && f[3] == t[6][3])
+               return "6"; 
+       else
+          if(f[0]==t[7][0] && f[1] == t[7][1] && f[2] == t[7][2] && f[3] == t[7][3])
+               return "7";
+       else
+          if(f[0]==t[8][0] && f[1] == t[8][1] && f[2] == t[8][2] && f[3] == t[8][3])
+               return "8";
+       else
+          if(f[0]==t[9][0] && f[1] == t[9][1] && f[2] == t[9][2] && f[3] == t[9][3])
+               return "9";
+        else
                return "?";             
     }             
     
